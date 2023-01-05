010382ef2715cab28370af75c6b8696b5dc0df9a
Adi Venkat <68367441+adivenkat05@users.noreply.github.com>
2023-01-05 22:57:56 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -3,14 +3,18 @@
 #include<stdio.h>
 int main()
 {
-    int a = 5, c;
-    int b = 10;
+    int a, b, temp;
+
+    printf("Enter the value for a: ");
+    scanf("%d", &a);
+    printf("Enter the value for b: ");
+    scanf("%d", &b);
 
     printf("The value of a and b: %d and %d\n", a, b);
 
-    c = a; // Value of a will be assigned to c.
+    temp = a; // Value of a will be assigned to temp.
     a = b; // Value of b will be assigned to a.
-    b = c; // Value of c will be assigned to b.
+    b = temp; // Value of temp will be assigned to b.
 
     printf("The value of a and b: %d and %d", a, b);
 }
